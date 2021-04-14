489fcbe13d2107b9a633cb46ce78abe0966fe7e5
Amanda <atuyen26@gmail.com>
2021-04-13 21:49:49 -0700

    add in button styling and onclick and run tests to confirm its working

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,29 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = 'button';
+   let onClick = `onClick ={action('button-clicked')}`
+
+   if (props.confirm) {
+      buttonClass += ' button--confirm';
+
+   }
+
+  if (props.danger) {
+      buttonClass += ' button--danger';
+   }
+
+   if (props.disabled) {
+      onClick = ` disabled ${onClick}`;
+   }
+   
+   return (
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
