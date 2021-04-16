f483a1d310de04eaa082ebe8f85d2ec1a9a2b63c
Amanda <atuyen26@gmail.com>
2021-04-16 03:12:51 -0700

    add in empty and header components

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Empty (props) {
+  <main className="appointment__add">
+    <img
+      className="appointment__add-button"
+      src="images/add.png"
+      alt="Add"
+      onClick={props.onAdd}
+    />
+  </main>
+}
+
+
--- stories/index.js
+++ stories/index.js
@@ -10,10 +10,11 @@ import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 
-import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -136,5 +137,10 @@ storiesOf("DayListItem", module)
           backgrounds: [{ name: "white", value: "#fff", default: true }]
         })
         .add("Appointment", () => <Appointment />)
-        .add("Appointment with time", () => <Appointment time="12pm" />);
+        .add("Appointment with time", () => <Appointment time="12pm" />)
+        .add("Header", () => <Header time="12pm" />)
+        .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+
+
+
 
