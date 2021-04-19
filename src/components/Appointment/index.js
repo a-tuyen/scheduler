9dc4c0e2c86f8905aee40721fc218bf52ef1d51a
Amanda <atuyen26@gmail.com>
2021-04-19 00:44:59 -0700

    add in header, appt empty and appt booked components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
+
+
 export default function Appointment (props) {
+
+//   return (
+//     <article className="appointment">
+//       <Header time={props.time} />
+//       {props.interview ? (
+//         <Show
+//           student={props.interview.student}
+//           interview={props.interview.interviewer}
+//         />
+//       ) : (
+//         <Empty />
+//       )}
+//     </article>
+//   );
+// }
+
   return (
     <article className="appointment">
-
+      <Header 
+      time={props.time}
+      />
+      { props.interview ?  <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty /> }
     </article>
   )
 }
+
+
+// All Appointment components will render a Header that 
+// takes in a time prop.
+// If props.interview is truthy (an interview object) 
+// the Appointment will render the <Show /> component, 
+// else it should render the <Empty /> component.
+
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -21,6 +21,7 @@ import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
 import Form from "components/Appointment/Form.js";
+
 
 
 storiesOf("Button", module)
@@ -153,6 +154,21 @@ storiesOf("DayListItem", module)
         .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
         .add("Form with Edit", () => <Form name="Lydia Miller-Jones" interviewers={interviewers} interviewer={interviewer.id} onSave={action("onSave")} onCancel={action("onCancel")} />)
         .add("Form with Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
-
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="12pm" />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
+        .add("Appointment Booked", () => (
+          <Fragment>
+            <Appointment
+              id={1}
+              time="12pm"
+              interview={{ student: "Lydia Miller-Jones", interviewer: interviewer.name }}
+            />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
 
 
