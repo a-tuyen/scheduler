36df36a0f6f6c1d5bcb5ef0e05b5a67e92ee11a2
Amanda <atuyen26@gmail.com>
2021-04-20 16:33:54 -0700

    install axios as dependency and grabs days from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 
@@ -6,24 +7,6 @@ import DayList from "components/DayList.js"
 
 import Appointment from "components/Appointment";
 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -74,7 +57,17 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const url = 'http://localhost:8001/api/days';
+    axios.get(url).then(response => {
+      console.log('response', response.data);
+      setDays(response.data)
+    });
+  }, [])
+
 
   const apptList = appointments.map(appointment => {
     return (
@@ -101,8 +94,8 @@ export default function Application(props) {
 <nav className="sidebar__menu">
 <DayList
   days={days}
-  day={day}
-  setDay={setDay}
+  day={props.day}
+  setDays={setDays}
 />
 </nav>
 <img
