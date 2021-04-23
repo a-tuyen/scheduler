ccada4a5b1687ffd9afff010fc33e95c21b0b5f0
Amanda <atuyen26@gmail.com>
2021-04-22 21:10:05 -0700

    completes the useVisialMode custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,29 +1,31 @@
+import Form from "components/Appointment/Form";
 import { useState } from "react";
 
 
 export default function useVisualMode(initial) {
     const [mode, setMode] = useState(initial);
     const [history, setHistory] = useState([initial]);
-    console.log('mode:', mode)
 
-    const transition = (newMode) => {
-      setMode(newMode);
-      console.log('Historystart:', history);
-      console.log('modeafter:', newMode);
+    const transition = (newMode, replace = false) => {
+      
+      if (replace) {
+        setHistory((prev) => {
+          let newHistory = prev.slice(0, -1);
+          return [...newHistory, newMode];
+        })
+      } else {
       setHistory(prev => prev = [...prev, newMode]);
     }
+    setMode(newMode);
+  };
 
      const back = () => {
        if (history.length > 1) {
          history.pop();
          setMode(history[history.length-1])
        }
-       
-       console.log('history:', history)
-       console.log('modeafterback:', mode);
-
      };
-
-
     return { mode, transition, back };
 }
+
+// Form, status(error), show
