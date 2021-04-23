553931957db551f2e4bbbb4f5f93d930ba16fb57
Amanda <atuyen26@gmail.com>
2021-04-23 02:52:23 -0700

    create getInterviewersForDay selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -22,3 +22,19 @@ export function getInterview(state, interview) {
   };
   return interviewObj;
 }
+
+export function getInterviewersForDay(state, day) {
+  const dayFound = state.days.find(eachDay => eachDay.name === day);
+
+  if (!dayFound) {
+    return [];
+  }
+
+  if (state.days.length === 0) {
+    return [];
+  }
+  console.log('DAYFOUND:', dayFound);
+  const interviewers = dayFound.interviewers.map(interviewerId => state.interviewers[interviewerId]);
+  console.log('INTERVIEWERS:', interviewers)
+  return interviewers;
+}
--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,6 +1,5 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
-
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -32,6 +31,13 @@ test("useVisualMode should return to previous mode", () => {
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
