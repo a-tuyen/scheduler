31b679492790746d61df9041ca8dce8dd78ed84e
Amanda <atuyen26@gmail.com>
2021-04-25 05:23:06 -0700

    add more tests to form. went from 63% to 94% coverage

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -27,7 +27,8 @@ export default function Form (props) {
       setError("Student name cannot be blank");
       return;
     }
-  
+    
+    setError("");
     props.onSave(name, interviewer);
   }
 
